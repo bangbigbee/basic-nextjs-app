8f9ca0d261fb078f1a9516c8668fad03c7a30b17
bangbigbee <bangdtbk@gmail.com>
2023-08-08 21:42:09 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,98 @@
-import Image from 'next/image'
+'use client'
+import Image from 'next/image';
+import React, { useState } from 'react';
+
 
 export default function Home() {
+
+  const [isTurnedOn, setIsTurnedOn] = useState(true);
+  const [log, setLog] = useState<{ time: string; value: string }[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const pageSize = 10; // Số dòng dữ liệu trên mỗi trang
+
+  const handleTurnOn = () => {
+    setIsTurnedOn(true);
+    addToLog('ON');
+  };
+
+  const handleTurnOff = () => {
+    setIsTurnedOn(false);
+    addToLog('OFF');
+  };
+
+  const addToLog = (value: string) => {
+    const newEntry = {
+      time: new Date().toLocaleTimeString(),
+      value: value,
+    };
+    setLog((prevLog) => [...prevLog, newEntry]);
+    if ((log.length + 1) % pageSize === 1) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+
+  const renderLog = () => {
+    const start = (currentPage - 1) * pageSize;
+    const end = start + pageSize;
+    return log.slice(start, end).map((entry, index) => (
+      <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
+        <td className="border p-2">{entry.time}</td>
+        <td className="border p-2">{entry.value}</td>
+      </tr>
+    ));
+  };
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative flex place-items-center ...">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
+          src={isTurnedOn ? '/lightingbulb.jpg' : '/lightbulb.jpg'}
+          alt={isTurnedOn ? "Lighting Bulb" : "Light Bulb"}
           width={180}
           height={37}
           priority
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex items-center space-x-4">
+        <button className="border rounded-lg px-4 py-2 transition-colors hover:border-blue-500" onClick={handleTurnOn}>
+          Turn on
+        </button>
+        <button className="border rounded-lg px-4 py-2 transition-colors hover:border-red-500" onClick={handleTurnOff}>
+          Turn off
+        </button>
       </div>
+      <div className="mt-4 border-collapse border border-gray-300">
+        <table className="w-full">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="border p-2 text-black">Time</th>
+              <th className="border p-2 text-black">Value</th>
+            </tr>
+          </thead>
+          <tbody>{renderLog()}</tbody>
+        </table>
+      </div>
+      <div className="mt-4">
+        <button
+          onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
+          disabled={currentPage === 1}
+          className="mr-2 bg-gray-200 px-2 py-1 rounded text-black"
+        >
+          Previous Page
+        </button>
+        <button
+          onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
+          disabled={(currentPage - 1) * pageSize + pageSize >= log.length}
+          className="bg-gray-200 px-2 py-1 rounded text-black"
+        >
+          Next Page
+        </button>
+      </div>
+
     </main>
-  )
+  );
 }
